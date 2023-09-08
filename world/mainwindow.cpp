d6ca73702dac29e0ab32e324312578e0e9296b26
Max <maxstrack@yahoo.com>
2023-09-08 10:54:14 -0600

    updates

--- world/mainwindow.cpp
+++ world/mainwindow.cpp
@@ -17,35 +17,24 @@ void MainWindow::on_engOut_textEdited(const QString &arg1) {
 }
 
 void MainWindow::convert(){
-    //sorted via most reoccurring
-    const QChar english[26] = {'e','t','a','o','i','n','s','h','r','d','l','c','u','m','w','f','g','y','p','b','v','k','j','x','q','z'};
-    const QString zentil[26] = {"a-","z-","eh-","i-","uh-","rr-","n-","s-","l-","v-","t-","m-","o-","th-","k-", "sh-","f-","veh-","za-","mi-","zi-","rreh-","ke-","irr-","tho-","so-"};
-
-    const std::vector<QString> end2{"ed","er"};
-    const std::vector<QString> end3{"ion","ing"};
-    const std::vector<std::vector<QString>> endAll{end2,end3};
-
-    const std::vector<QString> search2{"cc","ll","sh","th","oo","nn","tt","ee","rr","kn","wh"};
-    const std::vector<QString> search3{"ght"};
-    const std::vector<std::vector<QString>> searchAll{search2,search3};
-
-
     const QString engWord = ui->engOut->text();
     QList<QString> engSen;
     QString zenWord;
+
+    // Transforms the imputed sentence into a QList of Qstings
     int sepIndx = 0;
-    //QString word;
     while (sepIndx < engWord.length()) {
         int tmpIndx = engWord.indexOf(' ', sepIndx);
         if (tmpIndx == -1)
             tmpIndx = engWord.length();
-        //engSen.append(engWord.sliced(sepIndx, tmpIndx-sepIndx));
         QString tmpWord = engWord.sliced(sepIndx, tmpIndx-sepIndx);
         engSen.append(tmpWord);
         sepIndx = tmpIndx + 1;
     }
 
+    // Edits the word depending on replaceAll and endAll list in mainwindow.h
     for (int t = 0;t <engSen.length(); ++t){
+        //the word that we will be working on
         QString word = engSen.at(t);
         // trim the ending depending on the endAll list
         if (word.length() > 4){
@@ -56,25 +45,25 @@ void MainWindow::convert(){
                 }
             }
         }
-
-        // clears unwanted combinations
-        for (int i=2; i <= 3; ++i){
-            for (size_t j=0; j < searchAll[i-2].size(); ++j){
+        // replaces unwanted combinations via replaceAll list
+        for (int i=1; i <= 3; ++i){
+            for (size_t j=0; j < replaceAll[i-1].size(); ++j){
                 int index = 0;
                 while (index < word.length()){
-                    index = word.indexOf(searchAll[i-2][j], index);
+                    index = word.indexOf(replaceAll[i-1][j][0], index);
                     if (index == -1)
                         break;
-                    word.remove(index+1,1);
+                    word.replace(index,i, replaceAll[i-1][j][1]);
                     index += i;
                 }
             }
         }
+        // Save changes to engSen
         engSen.replace(t, word);
 
         // convert
         for (size_t i=0; i < word.length(); ++i){
-            for (int j=0; j < 26; ++j){
+            for (int j=0; j < 17; ++j){
                 if (word[i] == english[j]){
                     zenWord.append(zentil[j]);
                 }
@@ -91,40 +80,54 @@ void MainWindow::convert(){
 }
 
 void MainWindow::letterDisplay(const QList<QString> &eng) {
-    QPixmap a("../letters/a.png");
-    QPixmap eh("../letters/eh.png");
-    QPixmap f("../letters/f.png");
-    QPixmap i("../letters/i.png");
-    QPixmap k("../letters/k.png");
-    QPixmap l("../letters/l.png");
-    QPixmap m("../letters/m.png");
-    QPixmap n("../letters/n.png");
-    QPixmap o("../letters/o.png");
-    QPixmap rr("../letters/rr.png");
-    QPixmap s("../letters/s.png");
-    QPixmap sh("../letters/sh.png");
-    QPixmap t("../letters/t.png");
-    QPixmap th("../letters/th.png");
-    QPixmap uh("../letters/uh.png");
-    QPixmap v("../letters/v.png");
-    QPixmap z("../letters/z.png");
+    const QPixmap a("../letters/a.png");
+    const QPixmap eh("../letters/eh.png");
+    const QPixmap f("../letters/f.png");
+    const QPixmap i("../letters/i.png");
+    const QPixmap k("../letters/k.png");
+    const QPixmap l("../letters/l.png");
+    const QPixmap m("../letters/m.png");
+    const QPixmap n("../letters/n.png");
+    const QPixmap o("../letters/o.png");
+    const QPixmap rr("../letters/rr.png");
+    const QPixmap s("../letters/s.png");
+    const QPixmap sh("../letters/sh.png");
+    const QPixmap t("../letters/t.png");
+    const QPixmap th("../letters/th.png");
+    const QPixmap uh("../letters/uh.png");
+    const QPixmap v("../letters/v.png");
+    const QPixmap z("../letters/z.png");
 
-    QList imgList = {a,z,eh,i,uh,rr,n,s,l,v,t,m,o,th,k,sh,f};
-    const QChar english[26] = {'e','t','a','o','i','n','s','h','r','d','l','c','u','m','w','f','g','y','p','b','v','k','j','x','q','z'};
+    const QList imgList = {a,z,eh,i,uh,rr,n,s,l,v,t,m,o,th,k,sh,f};
 
-    QPixmap disp(14*a.width(), a.height());
-    disp.fill(Qt::transparent);
-    QPainter painter(&disp);
-    int width=0;
+    QPixmap empty(a.width(), a.height());
+    empty.fill(Qt::transparent);
+    int width = 0;
     for (int listIndx=0; listIndx<eng.length(); ++listIndx){
         for (int letterIndx=0; letterIndx<eng.at(listIndx).length(); ++letterIndx){
-            for (int alph=0; alph<26; ++alph){
-                if (eng.at(listIndx).at(letterIndx) == english[alph] && alph<17) {
+            for (int alph=0; alph<17; ++alph){
+                if (eng.at(listIndx).at(letterIndx) == english[alph]) {
+                    width += imgList.at(alph).width() -2;
+                }
+            }
+        }
+        width += a.width();
+    }
+    QPixmap disp(width, a.height());
+    disp.fill(Qt::transparent);
+    QPainter painter(&disp);
+    width=0;
+    for (int listIndx=0; listIndx<eng.length(); ++listIndx){
+        for (int letterIndx=0; letterIndx<eng.at(listIndx).length(); ++letterIndx){
+            for (int alph=0; alph<17; ++alph){
+                if (eng.at(listIndx).at(letterIndx) == english[alph]) {
                     painter.drawPixmap(width,0,imgList.at(alph));
                     width += imgList.at(alph).width() -2;
                 }
             }
         }
+        painter.drawPixmap(width,0,empty);
+        width += a.width();
     }
 
     if (! ui->graphicsView->scene()) {
